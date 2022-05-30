7603b20d0a3c4a7b141a62ad822f8bb6f3054af6
Meclotfi <hl_mecharbat@esi.dz>
2022-05-30 13:11:36 +0200

    Update mobilevit.py

--- mmdet/models/backbones/mobilevit.py
+++ mmdet/models/backbones/mobilevit.py
@@ -1,4 +1,4 @@
-from turtle import forward
+import torch.utils.checkpoint as cp
 import numpy as np
 from torch import nn, Tensor
 import math
@@ -11,7 +11,6 @@ from mmcv.cnn import ConvModule,build_norm_layer
 from torch import nn, Tensor
 from mmcv.cnn.bricks.registry import ACTIVATION_LAYERS,ATTENTION
 from mmcv.cnn.bricks.transformer import build_transformer_layer
-from ..utils import InvertedResidual
 from mmcv.utils import to_2tuple
 from mmcv.runner import BaseModule
 from ..builder import BACKBONES
@@ -31,6 +30,91 @@ if ACTIVATION_LAYERS.get("Swish") is None:
     class Swish(nn.SiLU):
         def __init__(self, inplace: bool = False):
             super(Swish, self).__init__(inplace=inplace)
+class InvertedResidual(BaseModule):
+    """InvertedResidual block for MobileNetV2.
+
+    Args:
+        in_channels (int): The input channels of the InvertedResidual block.
+        out_channels (int): The output channels of the InvertedResidual block.
+        stride (int): Stride of the middle (first) 3x3 convolution.
+        expand_ratio (int): adjusts number of channels of the hidden layer
+            in InvertedResidual by this amount.
+        conv_cfg (dict, optional): Config dict for convolution layer.
+            Default: None, which means using conv2d.
+        norm_cfg (dict): Config dict for normalization layer.
+            Default: dict(type='BN').
+        act_cfg (dict): Config dict for activation layer.
+            Default: dict(type='ReLU6').
+        with_cp (bool): Use checkpoint or not. Using checkpoint will save some
+            memory while slowing down the training speed. Default: False.
+
+    Returns:
+        Tensor: The output tensor
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride,
+                 expand_ratio,
+                 conv_cfg=None,
+                 norm_cfg=dict(type='BN'),
+                 act_cfg=dict(type='ReLU6'),
+                 with_cp=False,
+                 init_cfg=None):
+        super(InvertedResidual, self).__init__(init_cfg)
+        self.stride = stride
+        assert stride in [1, 2], f'stride must in [1, 2]. ' \
+            f'But received {stride}.'
+        self.with_cp = with_cp
+        self.use_res_connect = self.stride == 1 and in_channels == out_channels
+        hidden_dim = int(round(in_channels * expand_ratio))
+
+        layers = []
+        if expand_ratio != 1:
+            layers.append(
+                ConvModule(
+                    in_channels=in_channels,
+                    out_channels=hidden_dim,
+                    kernel_size=1,
+                    conv_cfg=conv_cfg,
+                    norm_cfg=norm_cfg,
+                    act_cfg=act_cfg))
+        layers.extend([
+            ConvModule(
+                in_channels=hidden_dim,
+                out_channels=hidden_dim,
+                kernel_size=3,
+                stride=stride,
+                padding=1,
+                groups=hidden_dim,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg,
+                act_cfg=act_cfg),
+            ConvModule(
+                in_channels=hidden_dim,
+                out_channels=out_channels,
+                kernel_size=1,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg,
+                act_cfg=None)
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        def _inner_forward(x):
+            if self.use_res_connect:
+                return x + self.conv(x)
+            else:
+                return self.conv(x)
+
+        if self.with_cp and x.requires_grad:
+            out = cp.checkpoint(_inner_forward, x)
+        else:
+            out = _inner_forward(x)
+
+        return out
 
 if ATTENTION.get("MultiScaleDeformableAttentionL1") is None:
     @ATTENTION.register_module()
@@ -528,12 +612,11 @@ class MobileViT(BaseModule):
         output_channels = cfg.get("out_channels")
         num_blocks = cfg.pop("num_blocks",1)
         stride=cfg.pop("stride",1)
-        expand_ratio=cfg.pop("expand_ratio",2)
         block = []
 
         for i in range(num_blocks):
             stride=stride if i==0 else 1
-            layer = InvertedResidual(in_channels=input_channel,stride=stride,mid_channels=int(round(input_channel * expand_ratio)),**cfg)
+            layer = InvertedResidual(in_channels=input_channel,stride=stride,**cfg)
             block.append(layer)
             input_channel = output_channels
         return nn.Sequential(*block), input_channel
@@ -552,7 +635,7 @@ class MobileViT(BaseModule):
                 in_channels=input_channel,
                 out_channels=cfg.get("out_channels"),
                 stride=stride,
-                mid_channels=int(round(input_channel * cfg.get("mv_expand_ratio", 2)))
+                expand_ratio=cfg.get("mv_expand_ratio", 2)
             )
 
             block.append(layer)
