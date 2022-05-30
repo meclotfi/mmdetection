3b4bb77bb11387857b56eeac380426a04641daee
Meclotfi <hl_mecharbat@esi.dz>
2022-05-30 13:00:00 +0200

    Update mobilevit_defdetr.py

--- configs/mobilevit/mobilevit_defdetr.py
+++ configs/mobilevit/mobilevit_defdetr.py
@@ -1,11 +1,12 @@
-base=["../_base_/datasets/vww_detection.py",'../_base_/default_runtime.py']
+base=['../_base_/datasets/vww_detection.py',
+'../_base_/default_runtime.py']
 model = dict(
     type='DeformableDETR',
     backbone=dict(
         type='MobileViT'),
     neck=dict(
         type='ChannelMapper',
-        in_channels=[48, 64, 80],
+        in_channels=[24, 48, 80],
         kernel_size=1,
         out_channels=32,
         act_cfg=None,
@@ -15,7 +16,7 @@ model = dict(
         type='DeformableDETRHead',
         num_query=50,
         num_classes=1,
-        in_channels=256,
+        in_channels=128,
         sync_cls_avg_factor=True,
         as_two_stage=False,
         transformer=dict(
@@ -26,7 +27,13 @@ model = dict(
                 transformerlayers=dict(
                     type='BaseTransformerLayer',
                     attn_cfgs=dict(
-                        type='MultiScaleDeformableAttention', embed_dims=64),
+                        type='MultiScaleDeformableAttention', embed_dims=32),
+                    ffn_cfgs=dict(
+                     type='FFN',
+                     embed_dims=32,
+                     feedforward_channels=128,
+                     num_fcs=2,
+                 ),
                     feedforward_channels=128,
                     ffn_dropout=0.1,
                     operation_order=('self_attn', 'norm', 'ffn', 'norm'))),
@@ -39,20 +46,26 @@ model = dict(
                     attn_cfgs=[
                         dict(
                             type='MultiheadAttention',
-                            embed_dims=64,
+                            embed_dims=32,
                             num_heads=8,
                             dropout=0.1),
                         dict(
                             type='MultiScaleDeformableAttention',
-                            embed_dims=64)
+                            embed_dims=32)
                     ],
+                    ffn_cfgs=dict(
+                     type='FFN',
+                     embed_dims=32,
+                     feedforward_channels=128,
+                     num_fcs=2,
+                 ),
                     feedforward_channels=128,
                     ffn_dropout=0.1,
                     operation_order=('self_attn', 'norm', 'cross_attn', 'norm',
                                      'ffn', 'norm')))),
         positional_encoding=dict(
             type='SinePositionalEncoding',
-            num_feats=32,
+            num_feats=16,
             normalize=True,
             offset=-0.5),
         loss_cls=dict(
@@ -70,4 +83,68 @@ model = dict(
             cls_cost=dict(type='FocalLossCost', weight=2.0),
             reg_cost=dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
             iou_cost=dict(type='IoUCost', iou_mode='giou', weight=2.0))),
-    test_cfg=dict(max_per_img=100))
+    test_cfg=dict(max_per_img=50))
+
+# dataset settings
+dataset_type = 'VwwDataset'
+data_root = 'data/coco/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 2)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+        min_crop_size=0.3),
+    dict(
+        type='Resize',
+        img_scale=(320, 320),
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(320, 320),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_person_ids="annotations/person_train2017.npy",
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox',metric_options={'topk': (1,)})
